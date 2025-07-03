34da7c1e20d9ba716244a250ad7309f30c0cecc3
schwartz <gtfsolo@gmail.com>
2025-07-03 21:20:28 +0300

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Update code 👽</p>
     </>
   );
 }
